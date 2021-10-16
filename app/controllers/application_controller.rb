79c83b99eccf04825b65efbb9d746ecef62dadec
James Chevalier <jchevalier@gmail.com>
2021-10-16 16:18:30 -0400

    prosopite configuration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action do
+    Prosopite.scan
+  end
+
+  after_action do
+    Prosopite.finish
+  end
+
   include Pagy::Backend
 end
